8a21e9015191614f3b4a9b1c4ffa20b06536803c
Syrus Akbary <me@syrusakbary.com>
2018-07-19 16:24:05 -0700

    Improved HttpQueryError hashing

--- graphql_server/error.py
+++ graphql_server/error.py
@@ -1,6 +1,3 @@
-import json
-
-
 class HttpQueryError(Exception):
     def __init__(self, status_code, message=None, is_graphql_error=False, headers=None):
         self.status_code = status_code
@@ -10,10 +7,17 @@ class HttpQueryError(Exception):
         super(HttpQueryError, self).__init__(message)
 
     def __eq__(self, other):
-        return isinstance(other, HttpQueryError) and \
-            other.status_code == self.status_code and \
-            other.message == self.message and \
-            other.headers == self.headers
+        return (
+            isinstance(other, HttpQueryError)
+            and other.status_code == self.status_code
+            and other.message == self.message
+            and other.headers == self.headers
+        )
 
     def __hash__(self):
-        return hash((self.status_code, self.message, json.dumps(self.headers)))
+        if self.headers:
+            headers_hash = tuple(self.headers.items())
+        else:
+            headers_hash = None
+
+        return hash((self.status_code, self.message, headers_hash))
