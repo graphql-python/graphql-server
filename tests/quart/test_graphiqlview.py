3f38a65db750ac6557a0834ac9d0aa5ef0a984da
KingDarBoja <stevenbojato04@gmail.com>
2020-10-28 19:24:40 -0500

    tests: refactor graphiqlview test suite

--- tests/quart/test_graphiqlview.py
+++ tests/quart/test_graphiqlview.py
@@ -1,6 +1,9 @@
+import sys
+
 import pytest
-from quart import Quart, url_for
+from quart import Quart, Response, url_for
 from quart.testing import QuartClient
+from werkzeug.datastructures import Headers
 
 from .app import create_app
 
@@ -22,20 +25,31 @@ def client(app: Quart) -> QuartClient:
 
 
 @pytest.mark.asyncio
+async def execute_client(
+    app: Quart,
+    client: QuartClient,
+    method: str = "GET",
+    headers: Headers = None,
+    **extra_params
+) -> Response:
+    if sys.version_info >= (3, 7):
+        test_request_context = app.test_request_context("/", method=method)
+    else:
+        test_request_context = app.test_request_context(method, "/")
+    async with test_request_context:
+        string = url_for("graphql", **extra_params)
+    return await client.get(string, headers=headers)
+
+
+@pytest.mark.asyncio
 async def test_graphiql_is_enabled(app: Quart, client: QuartClient):
-    async with app.test_request_context("/", method="GET"):
-        response = await client.get(
-            url_for("graphql", externals=False), headers={"Accept": "text/html"}
-        )
+    response = await execute_client(app, client, headers=Headers({"Accept": "text/html"}), externals=False)
     assert response.status_code == 200
 
 
 @pytest.mark.asyncio
 async def test_graphiql_renders_pretty(app: Quart, client: QuartClient):
-    async with app.test_request_context("/", method="GET"):
-        response = await client.get(
-            url_for("graphql", query="{test}"), headers={"Accept": "text/html"}
-        )
+    response = await execute_client(app, client, headers=Headers({"Accept": "text/html"}), query="{test}")
     assert response.status_code == 200
     pretty_response = (
         "{\n"
@@ -50,8 +64,7 @@ async def test_graphiql_renders_pretty(app: Quart, client: QuartClient):
 
 @pytest.mark.asyncio
 async def test_graphiql_default_title(app: Quart, client: QuartClient):
-    async with app.test_request_context("/", method="GET"):
-        response = await client.get(url_for("graphql"), headers={"Accept": "text/html"})
+    response = await execute_client(app, client, headers=Headers({"Accept": "text/html"}))
     result = await response.get_data(raw=False)
     assert "<title>GraphiQL</title>" in result
 
@@ -61,7 +74,6 @@ async def test_graphiql_default_title(app: Quart, client: QuartClient):
     "app", [create_app(graphiql=True, graphiql_html_title="Awesome")]
 )
 async def test_graphiql_custom_title(app: Quart, client: QuartClient):
-    async with app.test_request_context("/", method="GET"):
-        response = await client.get(url_for("graphql"), headers={"Accept": "text/html"})
+    response = await execute_client(app, client, headers=Headers({"Accept": "text/html"}))
     result = await response.get_data(raw=False)
     assert "<title>Awesome</title>" in result
