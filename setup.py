ce880b5bc5836930216ef6620eada877df44a825
Kien Dang <mail@kien.ai>
2023-10-03 21:48:40 +0800

    chore: add Python 3.12

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
     ],
     keywords="api graphql protocol rest",
