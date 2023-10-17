75a1d7d4d38cbbb834b3208455cd688014326107
Kien Dang <mail@kien.ai>
2023-10-17 16:04:46 +0800

    refactor!: Drop VersionInfo in favor of tuple (#124)
    * fix: fix and refactor version tests
    
    to handle release candidates correctly
    
    * fix: remove VersionInfo entirely
    
    keep graphql_server.version_info as a plain tuple
    add tests using the packaging package to make sure
    graphql_server.version and graphql_server.version_info are PEP 440–compliant and pointing to the same version
    
    * chore: update pre-commit hooks

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,78 +1,50 @@
-import re
+import packaging
+from packaging.version import Version
 
-import graphql_server
-from graphql_server.version import VersionInfo, version, version_info
+from graphql_server.version import version, version_info
 
-_re_version = re.compile(r"(\d+)\.(\d+)\.(\d+)(?:([abc])(\d+))?$")
-
-
-def test_create_version_info_from_fields():
-    v = VersionInfo(1, 2, 3, "alpha", 4)
-    assert v.major == 1
-    assert v.minor == 2
-    assert v.micro == 3
-    assert v.releaselevel == "alpha"
-    assert v.serial == 4
+RELEASE_LEVEL = {"alpha": "a", "beta": "b", "rc": "rc", "final": None}
 
 
-def test_create_version_info_from_str():
-    v = VersionInfo.from_str("1.2.3")
-    assert v.major == 1
-    assert v.minor == 2
-    assert v.micro == 3
-    assert v.releaselevel == "final"
-    assert v.serial == 0
-    v = VersionInfo.from_str("1.2.3a4")
-    assert v.major == 1
-    assert v.minor == 2
-    assert v.micro == 3
-    assert v.releaselevel == "alpha"
-    assert v.serial == 4
-    v = VersionInfo.from_str("1.2.3beta4")
-    assert v.major == 1
-    assert v.minor == 2
-    assert v.micro == 3
-    assert v.releaselevel == "beta"
-    assert v.serial == 4
-    v = VersionInfo.from_str("12.34.56rc789")
-    assert v.major == 12
-    assert v.minor == 34
-    assert v.micro == 56
-    assert v.releaselevel == "candidate"
-    assert v.serial == 789
+parsed_version = Version(version)
 
 
-def test_serialize_as_str():
-    v = VersionInfo(1, 2, 3, "final", 0)
-    assert str(v) == "1.2.3"
-    v = VersionInfo(1, 2, 3, "alpha", 4)
-    assert str(v) == "1.2.3a4"
+def test_valid_version() -> None:
+    packaging.version.parse(version)
 
 
-def test_base_package_has_correct_version():
-    assert graphql_server.__version__ == version
-    assert graphql_server.version == version
-
-
-def test_base_package_has_correct_version_info():
-    assert graphql_server.__version_info__ is version_info
-    assert graphql_server.version_info is version_info
-
-
-def test_version_has_correct_format():
-    assert isinstance(version, str)
-    assert _re_version.match(version)
-
-
-def test_version_info_has_correct_fields():
+def test_valid_version_info() -> None:
+    """version_info has to be a tuple[int, int, int, str, int]"""
     assert isinstance(version_info, tuple)
-    assert str(version_info) == version
-    groups = _re_version.match(version).groups()  # type: ignore
-    assert version_info.major == int(groups[0])
-    assert version_info.minor == int(groups[1])
-    assert version_info.micro == int(groups[2])
-    if groups[3] is None:  # pragma: no cover
-        assert groups[4] is None
-    else:  # pragma: no cover
-        assert version_info.releaselevel[:1] == groups[3]
-        assert version_info.serial == int(groups[4])
+    assert len(version_info) == 5
+
+    major, minor, micro, release_level, serial = version_info
+    assert isinstance(major, int)
+    assert isinstance(minor, int)
+    assert isinstance(micro, int)
+    assert isinstance(release_level, str)
+    assert isinstance(serial, int)
+
+
+def test_valid_version_release_level() -> None:
+    if parsed_version.pre is not None:
+        valid_release_levels = {v for v in RELEASE_LEVEL.values() if v is not None}
+        assert parsed_version.pre[0] in valid_release_levels
+
+
+def test_valid_version_info_release_level() -> None:
+    assert version_info[3] in RELEASE_LEVEL.keys()
+
+
+def test_version_same_as_version_info() -> None:
+    assert (
+        parsed_version.major,
+        parsed_version.minor,
+        parsed_version.micro,
+    ) == version_info[:3]
+
+    release_level, serial = version_info[-2:]
+    if parsed_version.is_prerelease:
+        assert (RELEASE_LEVEL[release_level], serial) == parsed_version.pre
+    else:
+        assert (release_level, serial) == ("final", 0)
